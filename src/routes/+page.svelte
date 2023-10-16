06efaca663e23e5109c18eb6cad4675bd1482437
Nico Krause <nico@le-space.de>
2023-10-16 10:41:34 +0500

    AI enabled and darkmode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,37 @@
 <script>
     import "carbon-components-svelte/css/all.css";
+    import Asleep from "carbon-icons-svelte/lib/Asleep.svelte";
+
+    import {
+        Theme,
+        Grid, Row, Column
+    } from "carbon-components-svelte";
+
     import {browser} from "$app/environment";
+    let theme = "g90";
 </script>
+
+
 <svelte:head>
     {#if browser}
         <script async src="https://cozycal.com/embed/v1/9375.js"></script>
     {/if}
 </svelte:head>
-
+<!--<div class="darkmode">-->
+<!--    <Asleep />-->
+<!--</div>-->
 <div class="container">
+    <Theme
+            render="toggle"
+            toggle={{
+    themes: ["g10", "g80"],
+    labelA: "Enable dark mode",
+    labelB: "Enable dark mode",
+    hideLabel: true,
+    size: "sm",
+  }}
+    />
+
     <div class="logo-container">
         <img src="/le-space-ug.png" alt="Le Space Logo" class="logo" />
     </div>
@@ -23,16 +46,27 @@
             When Le Space opened its doors, it wasn't just a coworking space—it was a vision of the future of work. Over the years, we've witnessed and shaped the rise of remote work and freelance culture, gathering unparalleled insights into the dynamics of collaborative environments, both physical and virtual.
         </p>
         <p>&nbsp;</p>
+        <p>
+            Today AI is joining our virtual remote coworking spaces around the globe. Again we need to adapt
+            and optimize our workflows leveraging the advantages of artificial intelligence.
+        </p>
+        <p>&nbsp;</p>
         <h2>Our Services:</h2>
         <p>&nbsp;</p>
         <ul>
             <li><strong>General IT- and Technology Consulting:</strong> With deep-rooted expertise in a diverse range of technologies, we provide bespoke solutions to businesses and individuals. Whether you're exploring new tech endeavors or refining existing systems, our team is equipped to guide you through the intricacies of the IT world.</li>
             <p>&nbsp;</p>
-            <li><strong>Peer Programming Sessions:</strong> Beyond traditional IT-consulting, we're proud to introduce our Peer Programming Sessions. These are specially designed for programmers and IT professionals, offering a collaborative platform to enhance skills, ensure code quality, and foster a culture of shared expertise.</li>
+            <li><strong>Peer Programming Sessions:</strong>
+                Beyond traditional IT-consulting, we're proud to introduce our Peer Programming Sessions.
+                These are specially designed for programmers and IT professionals, offering a collaborative platform to enhance skills, ensure code quality, and foster a culture of shared expertise.</li>
             <p>&nbsp;</p>
-           <li><strong>Remote Work Consulting:</strong> As remote work becomes a global norm, the challenges it brings to the forefront are equally universal. At Le Space, we offer strategies to manage, motivate, and coordinate distributed teams, ensuring seamless communication, workflow optimization, and work-life balance for remote employees.</li>
+           <li><strong>Remote Work Consulting:</strong>
+               As remote work becomes a global norm, the challenges it brings to the forefront are equally universal.
+               At Le Space, we offer strategies to manage, motivate, and coordinate distributed teams, ensuring seamless communication, workflow optimization, and work-life balance for remote employees.</li>
             <p>&nbsp;</p>
-            <li><strong>Coworking Consultation:</strong> Drawing from our rich history, we offer expert guidance to businesses and individuals on creating efficient and vibrant coworking spaces. We understand the nuances of space design, community building, and logistical management, ensuring a harmonious blend of productivity and collaboration.</li>
+            <li><strong>Coworking Consultation:</strong>
+                Drawing from our rich history, we offer expert guidance to businesses and individuals on creating efficient and vibrant coworking spaces.
+                We understand the nuances of space design, community building, and logistical management, ensuring a harmonious blend of productivity and collaboration.</li>
             <p>&nbsp;</p>
         </ul>
         <p>&nbsp;</p>
@@ -47,14 +81,17 @@
         font-family: "IBM Plex Sans", "Helvetica Neue", Arial, sans-serif;
         margin: 0;
         padding: 0;
-        background-color: #f4f4f4;
+        /*background-color: #f4f4f4;*/
     }
 
+    .darkmode {
+
+    }
     .container {
         max-width: 800px;
         margin: 50px auto;
         padding: 30px;
-        background-color: #fff;
+        /*background-color: #fff;*/
         box-shadow: 0 2px 20px rgba(0, 0, 0, 0.1);
         border-radius: 8px;
     }
