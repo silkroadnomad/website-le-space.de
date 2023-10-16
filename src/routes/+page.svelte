9b4a81ca08ea8eaff56d845edd1773076a8f0f4d
Nico Krause <nico@le-space.de>
2023-10-16 17:34:20 +0500

    speed up project timeline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,8 +20,8 @@
                 Whether you're exploring new tech endeavors or refining existing systems, we are equipped to guide you through the intricacies of the IT world. (more...)</li>
             <p>&nbsp;</p>
             <li><strong>Peer Programming Sessions:</strong><br><br>
-                Beyond traditional IT-consulting, we're proud to introduce our virtual Peer Programming Sessions starting with JDoc and e2e-Tests with Cypress.
-                These are specially designed for programmers and IT professionals, to ensure code quality, and foster a culture of shared expertise. (more...)</li>
+                Beyond traditional IT-consulting, we're proud to introduce our virtual Peer Programming Sessions starting in the near future with JDoc and e2e-Tests with Cypress.
+                These are specially designed for programmers and IT professionals, to ensure code quality and speed up the project time line. (more...)</li>
             <p>&nbsp;</p>
 <!--           <li><strong>Remote Work Consulting:</strong>
                As remote work becomes a global norm, the challenges it brings to the forefront are equally universal.
