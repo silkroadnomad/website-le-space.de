7260beb848b71fff910371274e2021f35a0d64a8
Nico Krause <nico@le-space.de>
2023-12-03 13:40:59 +0100

    changed

--- dist-le-space.de/_app/immutable/chunks/de.bbda0997.js
+++
@@ -1,1 +0,0 @@
-const e={home:{data_protection:"Datenschutz",imprint:"Impressum"},"info-panel":{year:"Jahr",projects:"Projekt(e)",tech:"Tech",industry:"Branche",location:"Location"}},t={page:e};export{t as default,e as page};
