1e8ee7508e2ebb729ddf23b68bef9eb7725f501b
Nico Krause <nico@le-space.de>
2024-03-04 02:03:11 +0800

    under construction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
     console.log("just a script!")
 </script>
 
-<h1> i am a tester</h1>
-<h1> i am a tester</h1><h1> i am a tester</h1><h1> i am a tester</h1><h1> i am a tester</h1><h1> i am a tester</h1>
+<!--<h1> i am a tester</h1>-->
+<!--<h1> i am a tester</h1><h1> i am a tester</h1><h1> i am a tester</h1><h1> i am a tester</h1><h1> i am a tester</h1>-->
